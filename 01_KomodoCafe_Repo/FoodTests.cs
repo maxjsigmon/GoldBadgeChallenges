9592aa51eefefc91b9205a5ada39156573ba3eb3
Max Sigmon <max.j.sigmon@gmail.com>
2020-10-28 15:06:14 -0400

    updates

--- 01_KomodoCafe_Repo/FoodTests.cs
+++ 01_KomodoCafe_Repo/FoodTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace _01_KomodoCafe_Repo
 {
     class FoodTests
     {
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
     }
 }
