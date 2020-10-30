3eeaefd77ee7ca4a397f50b2abd99c303aab1e62
Max Sigmon <max.j.sigmon@gmail.com>
2020-10-29 23:05:42 -0400

    added unit tests

--- 03_KomodoGreen_Tests/UnitTest1.cs
+++ 03_KomodoGreen_Tests/UnitTest1.cs
@@ -10,12 +10,54 @@ namespace _03_KomodoGreen_Tests
         [TestMethod]
         public void AddVehicleToEngineType_ShouldReturnTrue()
         {
-            ElectricType vehicle = new ElectricType();
+            ElectricType eVehicle = new ElectricType();
+            HybridType hVehicle = new HybridType();
+            GasType gVehicle = new GasType();
             VehicleRepository repository = new VehicleRepository();
 
-            bool addVehicle = repository.AddElectricCarToDirectories(vehicle);
+            bool addEVehicle = repository.AddElectricCarToDirectories(eVehicle);
+            bool addHVehicle = repository.AddHybridCarToDirectories(hVehicle);
+            bool addGVehicle = repository.AddGasCarToDirectories(gVehicle);
 
-            Assert.IsTrue(addVehicle);
+            Assert.IsTrue(addEVehicle);
+            Assert.IsTrue(addHVehicle);
+            Assert.IsTrue(addGVehicle);
+        }
+
+        [TestMethod]
+        public void DeleteCarInfo_ShouldReturnTrue()
+        {
+            VehicleRepository repo = new VehicleRepository();
+            ElectricType eVehicle = new ElectricType("Tesla", "Model X", 2020, EngineType.Electric, 300, 90);
+            repo.AddElectricCarToDirectories(eVehicle);
+
+            ElectricType oldEVehicle = repo.GetElectricCarByModel("Model X");
+
+            bool removeEVehicle = repo.DeleteElectricVehicleInfo(oldEVehicle);
+
+            Assert.IsTrue(removeEVehicle);
+
+            GasType gVehicle = new GasType("Ford", "Mustang", 2020, EngineType.Gas, 19);
+            repo.AddGasCarToDirectories(gVehicle);
+
+            GasType oldGVehicle = repo.GetGasCarByModel("Mustang");
+            bool removeGVehicle = repo.DeleteGasVehicleInfo(oldGVehicle);
+
+            Assert.IsTrue(removeGVehicle);
+        }
+
+        [TestMethod]
+        public void GetCarByModel_ShouldReturnCorrectName()
+        {
+            VehicleRepository repo = new VehicleRepository();
+            GasType vehicle = new GasType("Ford", "Mustang", 1999, EngineType.Gas, 15);
+            repo.AddGasCarToDirectories(vehicle);
+            string model = "Mustang";
+
+            GasType searchResult = repo.GetGasCarByModel(model);
+
+            Assert.AreEqual(searchResult.Model, model);
+
         }
     }
 }
